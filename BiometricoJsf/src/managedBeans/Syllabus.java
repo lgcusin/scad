221d128608c8cc38cf67ef877c89b1984bf98ae3
Wilson <wilson.dk713@gmail.com>
2019-05-09 11:44:03 -0500

    mas syllabus

--- BiometricoJsf/src/managedBeans/Syllabus.java
+++ BiometricoJsf/src/managedBeans/Syllabus.java
@@ -1,5 +1,6 @@
 package managedBeans;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
@@ -10,10 +11,14 @@ import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
+import javax.faces.event.ComponentSystemEvent;
+
+import org.primefaces.event.RowEditEvent;
 
 import model.Actividad;
 import model.Carrera;
 import model.Contenido;
+import model.Feriado;
 import model.Herramienta;
 import model.Materia;
 import model.Syllabo;
@@ -42,6 +47,26 @@ public class Syllabus {
 	public List<Herramienta> lstHrr;
 	public boolean dataSyllabo;
 	public boolean modiSyllabo;
+
+	public String tipAsignatura;
+	public String nmbDocente;
+	public String prdAcademico;
+	public int nmrHoras;
+	public int nmrTutorias;
+
+	public String descripcion;
+	public String objGeneral;
+	public String objEspecifico;
+	public String contribucion;
+	public String resultados;
+
+	public String uncrNombre;
+	public String uncrObjetivo;
+	public String uncrResultado;
+
+	public List<Contenido> lstContenidos;
+	public List<Actividad> lstActividads;
+	public List<Herramienta> lstherramientas;
 
 	@PostConstruct
 	public void init() {
@@ -109,6 +134,55 @@ public class Syllabus {
 	public String regresar() {
 		modiSyllabo = false;
 		return "syllabus";
+	}
+
+	public String regresarPrincipal() {
+		lstM = null;
+		return "principal";
+	}
+
+	public void limpiarSesion(ComponentSystemEvent event) {
+		lstM = null;
+
+	}
+
+	public void onAddNewCnt() {
+		lstContenidos = new ArrayList<>(1);
+		Contenido contenido = new Contenido();
+		lstContenidos.add(contenido);
+	}
+
+	public void onRowEditCnt(RowEditEvent event) {
+		actualizarContenido((Contenido) event.getObject());
+		FacesMessage msg = new FacesMessage("Registro editado", ((Contenido) event.getObject()).getCntId() + "");
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+	}
+
+	private void actualizarContenido(Contenido object) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void onAddNewAct() {
+		lstActividads = new ArrayList<>(1);
+		Actividad actividad = new Actividad();
+		lstActividads.add(actividad);
+	}
+
+	public void onRowEditAct(RowEditEvent event) {
+		actualizarActividad((Actividad) event.getObject());
+		FacesMessage msg = new FacesMessage("Registro editado", ((Actividad) event.getObject()).getActId() + "");
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+	}
+
+	private void actualizarActividad(Actividad object) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void onRowCancelCnt(RowEditEvent event) {
+		FacesMessage msg = new FacesMessage("Edicion cancelada", "");
+		FacesContext.getCurrentInstance().addMessage(null, msg);
 	}
 
 	// Setters and getters
@@ -185,6 +259,136 @@ public class Syllabus {
 
 	public void setDataSyllabo(boolean dataSyllabo) {
 		this.dataSyllabo = dataSyllabo;
+	}
+
+	/** Metodos para agregar datos del fomulario del sillabo **/
+
+	public String getTipAsignatura() {
+		return tipAsignatura;
+	}
+
+	public void setTipAsignatura(String tipAsignatura) {
+		this.tipAsignatura = tipAsignatura;
+	}
+
+	public String getNmbDocente() {
+		return nmbDocente;
+	}
+
+	public void setNmbDocente(String nmbDocente) {
+		this.nmbDocente = nmbDocente;
+	}
+
+	public String getPrdAcademico() {
+		return prdAcademico;
+	}
+
+	public void setPrdAcademico(String prdAcademico) {
+		this.prdAcademico = prdAcademico;
+	}
+
+	public int getNmrHoras() {
+		return nmrHoras;
+	}
+
+	public void setNmrHoras(int nmrHoras) {
+		this.nmrHoras = nmrHoras;
+	}
+
+	public int getNmrTutorias() {
+		return nmrTutorias;
+	}
+
+	public void setNmrTutorias(int nmrTutorias) {
+		this.nmrTutorias = nmrTutorias;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public String getObjGeneral() {
+		return objGeneral;
+	}
+
+	public void setObjGeneral(String objGeneral) {
+		this.objGeneral = objGeneral;
+	}
+
+	public String getObjEspecifico() {
+		return objEspecifico;
+	}
+
+	public void setObjEspecifico(String objEspecifico) {
+		this.objEspecifico = objEspecifico;
+	}
+
+	public String getContribucion() {
+		return contribucion;
+	}
+
+	public void setContribucion(String contribucion) {
+		this.contribucion = contribucion;
+	}
+
+	public String getResultados() {
+		return resultados;
+	}
+
+	public void setResultados(String resultados) {
+		this.resultados = resultados;
+	}
+
+	public String getUncrNombre() {
+		return uncrNombre;
+	}
+
+	public void setUncrNombre(String uncrNombre) {
+		this.uncrNombre = uncrNombre;
+	}
+
+	public String getUncrObjetivo() {
+		return uncrObjetivo;
+	}
+
+	public void setUncrObjetivo(String uncrObjetivo) {
+		this.uncrObjetivo = uncrObjetivo;
+	}
+
+	public String getUncrResultado() {
+		return uncrResultado;
+	}
+
+	public void setUncrResultado(String uncrResultado) {
+		this.uncrResultado = uncrResultado;
+	}
+
+	public List<Contenido> getLstContenidos() {
+		return lstContenidos;
+	}
+
+	public void setLstContenidos(List<Contenido> lstContenidos) {
+		this.lstContenidos = lstContenidos;
+	}
+
+	public List<Actividad> getLstActividads() {
+		return lstActividads;
+	}
+
+	public void setLstActividads(List<Actividad> lstActividads) {
+		this.lstActividads = lstActividads;
+	}
+
+	public List<Herramienta> getLstherramientas() {
+		return lstherramientas;
+	}
+
+	public void setLstherramientas(List<Herramienta> lstherramientas) {
+		this.lstherramientas = lstherramientas;
 	}
 
 }
